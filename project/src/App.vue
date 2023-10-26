c3fd43a72177de4030ed41a35cba4ddc40a09f42
AnMeAl <134971639+AnMeAl@users.noreply.github.com>
2023-10-26 19:10:12 +0300

    Загрузка кода

---
+++ project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='square'></div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.square {
+  background-color: red;
+  height: 40px;
+  width: 40px;
+}
+</style>
